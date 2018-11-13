013f7a9af18dab189aad87f2a5d775455f7fca2d
JeiWolfei <jvampy22@gmail.com>
2018-11-13 13:33:22 -0800

    about us bio

--- js/about.js
+++ js/about.js
@@ -3,29 +3,19 @@ import html from './html.js';
 function makeTemplate() {
     return html`
         
-    <div class="flex-container">
-  <div>1</div>
-  <div>2</div>
-  <div>3</div>
-</div>
-    
-    
-
-        <div>Kristin Hortsch</div>
+        <h2>Kristin Hortsch</h2>
         <img src="images/kristin.jpeg" alt="photo-of-kristin"/>
-        <p>Kristin is.....learning to code at Alchemy Code.</p>
         
         <h2>Ivan Poblete</h2>
         <img src="images/Ivan.jpeg" alt="photo-of-Ivan"/>
-        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
         
         <h2>Jei Shepherd</h2>
         <img src="images/Jei.jpeg" alt="photo-of-Jei"/>
-        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
         
         <h2>Connor Frendt</h2>
         <img src="images/Connor.jpeg" alt="photo-of-Connor"/>
-        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
+        
+        <p>We are a team of Mad Labbers,..... </p>
     `;
 }
 
