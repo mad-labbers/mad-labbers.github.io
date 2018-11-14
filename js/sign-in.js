a6f05a8c0085bc1a33005538c0787155ff781b30
Connor Frendt <cjacksonfx@yahoo.com>
2018-11-13 16:10:39 -0800

    styling fonts etc.

--- js/sign-in.js
+++ js/sign-in.js
@@ -6,18 +6,24 @@ function makeTemplate() {
     return html `
     <h1>Sign In</h1>
     <form id="user-form">
-        <label for="name">Name:</label>
-        <input type="text" id="name" name="name">
-            
-        <input type="radio" id="piranha" name="story" value="piranha" required />
-        <label for="piranha">How to Cross a Piranha-Infested River</label>
-        
-        <input type="radio" id="wedding" name="story" value="wedding" required />
-        <label for="wedding">Wedding Advice for the Happy Couple</label>
-
-        <input type="radio" id="alchemy" name="story" value="alchemy" required />
-        <label for="alchemy">Dragon</label>
-
+        <div>
+            <label for="name">Name:</label>
+            <input type="text" id="name" name="name">
+        </div>
+        <div class="themes">
+            <div id="theme-1">
+                <input type="radio" id="piranha" name="story" value="piranha" required />
+                <label for="piranha">How to Cross a Piranha-Infested River</label>
+            </div>
+            <div id="theme-2">
+                <input type="radio" id="wedding" name="story" value="wedding" required />
+                <label for="wedding">Wedding Advice for the Happy Couple</label>
+            </div>
+            <div id="theme-3">
+                <input type="radio" id="alchemy" name="story" value="alchemy" required />
+                <label for="alchemy">Dragon</label>
+            </div>
+        </div>
         <div class="button">
             <button id="play-button">Play!</button>
         </div>
