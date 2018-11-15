f2e69097e9b434a5dde4a5d49ab4662b397dfd58
JeiWolfei <jvampy22@gmail.com>
2018-11-15 10:56:13 -0800

    added story 5

--- js/input5.js
+++ js/input5.js
@@ -1,34 +1,191 @@
 import html from './html.js';
+import inputApi from '../data/input-api.js';
 
-let template = function(userResponse) {
-    return html` 
-        <div class="Pirates">
-            <h1>The Force</h1> 
-                <p>The Force is a mystical,  ${userResponse.adjective} power. As Jedi Master
-                 Obi-Wan Kenobi once said, "The Force is an energy field, created
-                 by all living ${userResponse.pluralnoun} , that surrounds us, penetrates us,
-                 and binds the ${userResponse.noun} together.' Using the power of the force, 
-                 a Jedi can do many ${userResponse.adjective2} things, like
-                 using the Force to exercise ${userResponse.bodypart} control over
-                 ${userResponse.adjective3}-minded ${userResponse.pluralnoun2}. A Jedi can also
-                 use the Force to move objects with his or her ${userResponse.bodypart2}. 
-                 It doesn't matter how ${userResponse.pluraladjective4} these objects are; it
-                 only matters how ${userResponse.adverb} the Jedi believes in the Force.
-                 Most importantly, the Force teaches a Jedi t reply on his or her feelings.
-                 As Obi-Wan Kenobi told his student Luke, ${userResponse.noun}-walker: "Your
-                 ${userResponse.bodypart3} can deceive you. Don't trust them. "Instead
-                 a Jedi should ${userResponse.adverb2} trust in the Force.
-                 </p>
-        </div>
-        `;
-};
+function makeTemplate() {
+    return html`
+        <form id="game">
+            <fieldset>
+                <h1>Enter a Word</h1>
+                <div class="input form">
 
-export default class Story5 {
-    constructor(userResponse) {
-        this.userResponse = userResponse;
+                    <div>
+                        <label for="noun">noun</label>
+                        <input class="word" type="text" name="noun" id="noun">
+                    </div>
+
+                    <div>    
+                        <label for="wackystate">Wacky State</label>
+                        <input class="word" type="wackystate" name="wackystate" id="wackystate">
+                    </div>
+
+                    <div>    
+                        <label for="verb">verb</label>
+                        <input class="word" type="text" name="verb" id="verb">
+                    <div>
+
+                    <div>    
+                        <label for="noun2">noun</label>
+                        <input class="word" type="text" name="noun2" id="noun2">
+                    </div>
+
+                    <div>    
+                        <label for="propername">Proper Name</label>
+                        <input class="word" type="text" name="propername" id="propername">
+                    </div>
+
+                    <div>    
+                        <label for="verb2">verb</label>
+                        <input class="word" type="text" name="verb2" id="verb2">
+                    </div>
+
+                    <div>    
+                        <label for="noun3">noun</label>
+                        <input class="word" type="text" name="noun3" id="noun3">
+                    </div>
+
+                    <div>    
+                        <label for="verb3">verb</label>
+                        <input class="word" type="text" name="verb3" id="verb3">
+                    </div>
+
+                    <div>    
+                        <label for="noun4">noun</label>
+                        <input class="word" type="text" name="noun4" id="noun4">
+                    </div>
+
+                    <div>    
+                        <label for="bodypart">bodypart</label>
+                        <input class="word" type="text" name="bodypart" id="bodypart">
+                    </div>
+
+                    <div>
+                        <label for="city">city</label>
+                        <input class="word" type="text" name="city" id="city">
+                    </div>
+
+                    <div>    
+                        <label for="activity">activity</label>
+                        <input class="word" type="text" name="activity" id="activity">
+                    </div>
+
+                    <div>
+                        <label for="chainrestaurant">chain restaurant</label>
+                        <input class="word" type="text" name="chainrestaurant" id="chainrestaurant">
+                    </div>
+
+                    <div>
+                        <label for="verbpasttense">verb past tense</label>
+                        <input class="word" type="text" name="verbpasttense" id="verbpasttense">
+                    </div>
+
+                    <div>
+                        <label for="month">month</label>
+                        <input class="word" type="text" name="month" id="month">
+                    </div>
+
+                    <div>
+                        <label for="verb4">verb</label>
+                        <input class="word" type="text" name="verb4" id="verb4">
+                    </div>
+
+                    <div>
+                        <label for="noun4">noun4</label>
+                        <input class="word" type="text" name="noun4" id="noun4">
+                    </div>
+
+                    <div>
+                        <label for="verbpasttense2">verb past tense</label>
+                        <input class="word" type="text" name="verbpasttense2" id="verbpasttense2">
+                    </div>
+
+                    <div>
+                        <label for="adjective">adjective</label>
+                        <input class="word" type="text" name="adjective" id="adjective">
+                    </div>
+
+                    <div>
+                        <label for="typeofworkers">type of workers</label>
+                        <input class="word" type="text" name="typeofworkers" id="typeofworkers">
+                    </div>
+
+                    <div>
+                        <label for="noun5">noun</label>
+                        <input class="word" type="text" name="noun5" id="noun5">
+                    </div>
+
+                    <div>
+                        <label for="verbpasttense3">verb past tense</label>
+                        <input class="word" type="text" name="verbpasttense3" id="verbpasttense3">
+                    </div>
+
+                    <div>
+                        <label for="famousperson">famous person</label>
+                        <input class="word" type="text" name="famousperson" id="famousperson">
+                    </div>
+
+                    <div>
+                        <label for="noun6">noun</label>
+                        <input class="word" type="text" name="noun6" id="noun6">
+                    </div>
+
+                    <div>
+                        <label for="personintheroom">person in the room</label>
+                        <input class="word" type="text" name="personintheroom" id="personintheroom">
+                    </div>
+
+                </div>
+
+            </fieldset>
+
+            <button type="submit">Submit</button>
+        </form>
+    `;
+}
+
+export default class Game5 {
+    constructor() {
+        this.userResponses = inputApi.getAll();
     }
+    
     render() {
-        let dom = template(this.userResponse);
+        const dom = makeTemplate();
+        const form = dom.querySelector('form');
+        form.addEventListener('submit', function(event) {
+            event.preventDefault();
+            const elements = form.elements;
+            const userResponse = {};
+
+            userResponse.noun = elements.noun.value;
+            userResponse.wackystate = elements.wackystate.value;
+            userResponse.verb = elements.verb.value;
+            userResponse.noun2 = elements.noun2.value;
+            userResponse.propername = elements.propername.value;
+            userResponse.verb2 = elements.verb2.value;
+            userResponse.noun3 = elements.noun3.value;
+            userResponse.verb3 = elements.verb3.value;
+            userResponse.noun4 = elements.noun4.value;
+            userResponse.bodypart = elements.bodypart.value;
+            userResponse.city = elements.city.value;
+            userResponse.activity = elements.activity.value;
+            userResponse.chainrestaurant = elements.chainrestaurant.value;
+            userResponse.verbpasttense = elements.verbpasttense.value;
+            userResponse.month = elements.month.value;
+            userResponse.verb4 = elements.verb4.value;
+            userResponse.noun4 = elements.noun4.value;
+            userResponse.verbpasttense2 = elements.verbpasttense2.value;
+            userResponse.adjcetive = elements.adjcetive.value;
+            userResponse.typeofworkers = elements.typeofworkers.value;
+            userResponse.noun5 = elements.noun5.value;
+            userResponse.verbpasttense3 = elements.verbpasttense3.value;
+            userResponse.famousperson = elements.famousperson.value;
+            userResponse.noun6 = elements.noun6.value;
+            userResponse.personintheroom = elements.personintheroom.value;
+            
+            
+            inputApi.add(userResponse);
+            
+            window.location = './results.html';
+        });
         return dom;
     }
 }
--- js/story5.js
+++ js/story5.js
@@ -2,10 +2,28 @@ import html from './html.js';
 
 let template = function(userResponse) {
     return html` 
-        <div class="dragon">
-            <h1>Caution: Dragon</h1> 
-                <p>The ${userResponse.color} Dragon is the ${userResponse.superlative} Dragon of all. It has ${userResponse.adjective1} ${userResponse['body-part-plural']}, and a ${userResponse['body-part']} shaped like a ${userResponse.noun1}. It loves to eat ${userResponse['animal-plural']}, although it will feast on nearly anything. It is ${userResponse.adjective2} and ${userResponse.adjective3}. You must be ${userResponse.adjective4} around it, or you may end up as its meal!</p>
-        </div>
+        <div class="news">
+            <h1>News of the Weird</h1> 
+                <p>
+                    A ${userResponse.noun} in ${userResponse.wackystate} was arrested this morning
+                    after he ${userResponse.verb} in front of ${userResponse.noun2} . ${userResponse.propername}
+                    had a history of ${userResponse.verb2}, but no one-not even his ${userResponse.noun3}
+                    -ever imagined he'd ${userResponse.verb3} with a ${userResponse.noun4} stuck in his
+                    ${userResponse.bodypart}.<br />
+                    Today in ${userResponse.city}, after a brief ${userResponse.activity}, cops followed an armed 
+                    man into a ${userResponse.chainrestaurant}. There he reportedly ${userResponse.verbpasttense}
+                    in the fry machine. <br />
+                    In ${userResponse.month}, a woman was charged with a strange crime. She ${userResponse.verb4} 
+                    with a ${userResponse.noun4}, she was ${userResponse.verbpasttense2} with a
+                    ${userResponse.adjective} dog. <br />
+                    And finally today in Portland a team of ${userResponse.typeofworkers} were found trapped in 
+                    a ${userResponse.noun5}. Apparently they been ${userResponse.verbpasttense3} when they discovered their
+                    boss was really ${userResponse.famousperson}. The team hid in a ${userResponse.noun6} but soon 
+                    realized they were stuck and had to wait for ${userResponse.personintheroom} to save them.
+                </p>
+
+            <audio autoplay></audio>
+            </div>
         `;
 };
 
@@ -15,6 +33,9 @@ export default class Story5 {
     }
     render() {
         let dom = template(this.userResponse);
+        let sound = dom.querySelector('audio');
+        sound.src = '../sounds/.mp3';
+        dom.appendChild(sound);
         return dom;
     }
 }
